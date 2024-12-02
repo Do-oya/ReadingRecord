29741451aa9ebe5b093e8b5762fcad7126e5236f
Do-oya <qkrwjdgus1441@naver.com>
2024-12-02 15:28:32 +0900

    아이템 12. toString을 항상 재정의하라

--- Effective-Java/src/test/java/item12/Item12.java
+++ Effective-Java/src/test/java/item12/Item12.java
@@ -1,7 +1,14 @@
 package item12;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("아이템 12. toString을 항상 재정의하라")
 public class Item12 {
+
+    @Test
+    void item12_test_1() {
+        PhoneNumber phoneNumber = new PhoneNumber(707, 867, 5309);
+        System.out.println(phoneNumber);
+    }
 }
