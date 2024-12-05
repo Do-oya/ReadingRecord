e55c28e54068f569b71267bf7e6585c8f7bdd0d3
Do-oya <qkrwjdgus1441@naver.com>
2024-12-05 17:12:14 +0900

    아이템 26. 로 타입은 사용하지 말라

---
+++ Effective-Java/src/main/java/chapter5/item26/Raw.java
@@ -0,0 +1,8 @@
+package chapter5.item26;
+
+import java.awt.*;
+
+public class Raw {
+    public static void unsafeAdd(List list, Object o) {
+    }
+}
---
+++ Effective-Java/src/test/java/chapter5/item26/Item26.java
@@ -0,0 +1,13 @@
+package chapter5.item26;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("아이템 26. 로 타입은 사용하지 말라")
+public class Item26 {
+
+    @Test
+    @DisplayName("런타임에 실패한다 - unsafeAdd 메서드가 로 타입(List)을 사용")
+    void item26_test_1() {
+    }
+}
