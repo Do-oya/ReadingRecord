509a06e21d1391324ca4f2f721feb0798d194f70
Do-oya <qkrwjdgus1441@naver.com>
2024-12-10 14:45:25 +0900

    아이템 44. 표준 함수형 인터페이스를 사용하라

--- Effective-Java/src/test/java/chapter7/item44/Item44.java
+++ Effective-Java/src/test/java/chapter7/item44/Item44.java
@@ -1,4 +1,13 @@
 package chapter7.item44;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("아이템 44. 표준 함수형 인터페이스를 사용하라")
 public class Item44 {
+
+    @Test
+    @DisplayName("")
+    void item44_test_1() {
+    }
 }
