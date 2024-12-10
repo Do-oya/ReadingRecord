74128e4327c74bdb47291c4981895ffff24f96b4
Do-oya <qkrwjdgus1441@naver.com>
2024-12-10 14:45:04 +0900

    아이템 43. 람다보다는 메서드 참조를 사용하라

--- Effective-Java/src/test/java/chapter7/item43/Item43.java
+++ Effective-Java/src/test/java/chapter7/item43/Item43.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("아이템 43. 람다보다는 메서드 참조를 사용하라")
 public class Item43 {
-    
+
     @Test
     void item43_test_1() {
     }
---
+++ Effective-Java/src/test/java/chapter7/item44/Item44.java
@@ -0,0 +1,4 @@
+package chapter7.item44;
+
+public class Item44 {
+}
