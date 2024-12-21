279aa633ecb46d304eec8b48a55293b351e777d4
Do-oya <qkrwjdgus1441@naver.com>
2024-12-21 13:47:06 +0900

    1장 Junit 시작하기

---
+++ Junit-in-action/src/main/java/com/book/junitinaction/JunitInActionApplication.java
@@ -0,0 +1,13 @@
+package com.book.junitinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitInActionApplication.class, args);
+    }
+
+}
