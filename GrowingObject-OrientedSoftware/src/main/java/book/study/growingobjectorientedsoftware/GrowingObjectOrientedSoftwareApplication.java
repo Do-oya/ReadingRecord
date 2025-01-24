427b0a194dd429fad25ce86f19e316824d8ceb40
Do-oya <qkrwjdgus1441@naver.com>
2025-01-24 22:02:45 +0900

    9장 경매 스나이퍼 개발 의뢰

---
+++ GrowingObject-OrientedSoftware/src/main/java/book/study/growingobjectorientedsoftware/GrowingObjectOrientedSoftwareApplication.java
@@ -0,0 +1,13 @@
+package book.study.growingobjectorientedsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowingObjectOrientedSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowingObjectOrientedSoftwareApplication.class, args);
+    }
+
+}
---
+++ GrowingObject-OrientedSoftware/src/test/java/book/study/growingobjectorientedsoftware/GrowingObjectOrientedSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package book.study.growingobjectorientedsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowingObjectOrientedSoftwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
