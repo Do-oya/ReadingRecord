54b5114178f70ffe109e91d76b3b65cf05ef0d4c
Do-oya <qkrwjdgus1441@naver.com>
2025-02-12 21:52:20 +0900

    테스트 실행전 DB 초기화 후 테스트 메서드 실행

--- tobySpring/src/test/java/com/study/tobyspring/user/dao/UserDaoTest.java
+++ tobySpring/src/test/java/com/study/tobyspring/user/dao/UserDaoTest.java
@@ -1,6 +1,7 @@
 package com.study.tobyspring.user.dao;
 
 import com.study.tobyspring.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
@@ -9,25 +10,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserDaoTest {
 
-    @Test
-    void add_test() throws SQLException, ClassNotFoundException {
-        UserDao dao = new UserDao();
+    private final UserDao dao = new UserDao();
 
+    @BeforeEach
+    void setUp() throws SQLException, ClassNotFoundException {
+        dao.resetTable();
+    }
+
+    @Test
+    void test() throws SQLException, ClassNotFoundException {
         User user = new User();
         user.setId("whiteship");
         user.setName("백기선");
         user.setPassword("married");
 
         dao.add(user);
-    }
 
-    @Test
-    void get_test() throws SQLException, ClassNotFoundException {
-        UserDao dao = new UserDao();
-
-        User user = dao.get("whiteship");
-        assertThat(user.getId()).isEqualTo("whiteship");
-        assertThat(user.getName()).isEqualTo("백기선");
-        assertThat(user.getPassword()).isEqualTo("married");
+        User user1 = dao.get(user.getId());
+        assertThat(user1.getId()).isEqualTo("whiteship");
+        assertThat(user1.getName()).isEqualTo("백기선");
+        assertThat(user1.getPassword()).isEqualTo("married");
     }
 }
