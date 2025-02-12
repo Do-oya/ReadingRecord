0d4e840a52030e5c44703d81e750a2331c732239
Do-oya <qkrwjdgus1441@naver.com>
2025-02-12 16:47:04 +0900

    토비의 스프링 공부

--- springbook/src/main/java/toby/springbook/user/service/UserLevelUpgradePolicy.java
+++
@@ -1,8 +0,0 @@
-package toby.springbook.user.service;
-
-import toby.springbook.user.domain.User;
-
-public interface UserLevelUpgradePolicy {
-    boolean canUpgradeLevel(User user);
-    void upgradeLevel(User user);
-}
