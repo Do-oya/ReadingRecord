200da2bcb9507be0f1decb410de7877d76ea268d
Do-oya <qkrwjdgus1441@naver.com>
2025-02-12 21:35:08 +0900

    JDBC를 이용한 등록과 조회 기능이 있는 UserDao 클래스

--- tobySpring/src/main/java/com/study/tobyspring/user/dao/UserDao.java
+++ tobySpring/src/main/java/com/study/tobyspring/user/dao/UserDao.java
@@ -2,10 +2,47 @@ package com.study.tobyspring.user.dao;
 
 import com.study.tobyspring.user.domain.User;
 
+import java.sql.*;
+
 public class UserDao {
 
-    public void add(User user) {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/springbook", "root", "1441");
 
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users(id, name, password) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
 
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/springbook", "root", "1441");
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
     }
 }
