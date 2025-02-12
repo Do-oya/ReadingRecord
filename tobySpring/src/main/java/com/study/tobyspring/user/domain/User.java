791266d581fda40bd38003dae32566380c2ac643
Do-oya <qkrwjdgus1441@naver.com>
2025-02-12 21:08:42 +0900

    JPA 사용 안 하고 진행

--- tobySpring/src/main/java/com/study/tobyspring/user/domain/User.java
+++ tobySpring/src/main/java/com/study/tobyspring/user/domain/User.java
@@ -1,19 +1,13 @@
 package com.study.tobyspring.user.domain;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.*;
 
-@Entity
 @Data
 public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
-    private String password;
+    String id;
+    String name;
+    String password;
 }
