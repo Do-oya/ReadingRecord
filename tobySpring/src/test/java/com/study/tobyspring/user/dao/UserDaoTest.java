cc069ebae2c6ba751f1d26272f9680233bd5a110
Do-oya <qkrwjdgus1441@naver.com>
2025-02-12 21:35:18 +0900

    UserDao 클래스 테스트 작성

--- tobySpring/src/test/java/com/study/tobyspring/user/dao/UserDaoTest.java
+++ tobySpring/src/test/java/com/study/tobyspring/user/dao/UserDaoTest.java
@@ -1,12 +1,33 @@
 package com.study.tobyspring.user.dao;
 
+import com.study.tobyspring.user.domain.User;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserDaoTest {
 
     @Test
-    void test() {
-        UserDao userDao = new UserDao();
-        userDao.add();
+    void add_test() throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+    }
+
+    @Test
+    void get_test() throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = dao.get("whiteship");
+        assertThat(user.getId()).isEqualTo("whiteship");
+        assertThat(user.getName()).isEqualTo("백기선");
+        assertThat(user.getPassword()).isEqualTo("married");
     }
 }
