acc023a64b3a2e0c54cfa0d0251e137340df688b
Do-oya <qkrwjdgus1441@naver.com>
2025-02-13 13:34:58 +0900

    직접 생성한 DaoFactory 오브젝트 출력 코드

---
+++ tobySpring/src/test/java/com/study/tobyspring/user/dao/DaoFactoryTest.java
@@ -0,0 +1,20 @@
+package com.study.tobyspring.user.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Import;
+
+@Import(DaoFactory.class)
+class DaoFactoryTest {
+
+    @Test
+    void test() {
+        DaoFactory factory = new DaoFactory();
+        UserDao dao1 = factory.userDao();
+        UserDao dao2 = factory.userDao();
+
+        System.out.println(dao1);
+        System.out.println(dao2);
+    }
+}
