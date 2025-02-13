6fee58eef1dc0643918f91c0a8af4ea7d0c4782e
Do-oya <qkrwjdgus1441@naver.com>
2025-02-13 10:16:20 +0900

    커넥션 로직 추가

--- tobySpring/src/main/java/com/study/tobyspring/user/dao/DConnectionMaker.java
+++ tobySpring/src/main/java/com/study/tobyspring/user/dao/DConnectionMaker.java
@@ -1,11 +1,14 @@
 package com.study.tobyspring.user.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DConnectionMaker implements ConnectionMaker {
 
     public Connection makeConnection() throws ClassNotFoundException, SQLException {
-        return null;
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/springbook", "root", "1441");
     }
 }
