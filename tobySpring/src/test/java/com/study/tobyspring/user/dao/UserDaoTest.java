a0283857e868f4df88438c9a820f0f7aa48e3c85
Do-oya <qkrwjdgus1441@naver.com>
2025-02-13 23:19:24 +0900

    스프링 테스트 컨텍스트를 적용한 UserDaoTest

--- tobySpring/src/test/java/com/study/tobyspring/user/dao/UserDaoTest.java
+++ tobySpring/src/test/java/com/study/tobyspring/user/dao/UserDaoTest.java
@@ -3,23 +3,31 @@ package com.study.tobyspring.user.dao;
 import com.study.tobyspring.user.domain.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {DaoFactory.class})
 public class UserDaoTest {
+
+    @Autowired
+    private ApplicationContext context;
 
     private UserDao dao;
     private User user;
 
     @BeforeEach
     void setUp() throws SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         this.dao = context.getBean("userDao", UserDao.class);
         this.user = new User("baby", "아사", "monster");
         dao.deleteAll();
