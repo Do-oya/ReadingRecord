739d63ed1776ea7b5db7dbf3545b7b1762ba5a1f
Do-oya <qkrwjdgus1441@naver.com>
2025-02-13 15:30:22 +0900

    주석 처리

--- tobySpring/src/main/java/com/study/tobyspring/user/dao/CountingDaoFactory.java
+++ tobySpring/src/main/java/com/study/tobyspring/user/dao/CountingDaoFactory.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CountingDaoFactory {
 
-    @Bean
+    /*@Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
-    }
+    }*/
 
     @Bean
     public ConnectionMaker connectionMaker() {
