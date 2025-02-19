a8461241f0e2d409cb397001ebb6d17e2b3663e6
Do-oya <qkrwjdgus1441@naver.com>
2025-02-19 22:46:46 +0900

    try/catch/finally를 적용한 calcSum()

---
+++ tobySpring/src/main/java/com/study/tobyspring/learningtest/template/Calculator.java
@@ -0,0 +1,34 @@
+package com.study.tobyspring.learningtest.template;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Calculator {
+    public Integer calcSum(String filePath) throws IOException {
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            int sum = 0;
+            String line;
+            while ((line = br.readLine()) != null) {
+                sum += Integer.parseInt(line);
+            }
+            return sum;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+        finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+}
