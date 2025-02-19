6526c53a11cfac6e003ef9bbe414b8b6dc415003
Do-oya <qkrwjdgus1441@naver.com>
2025-02-19 15:34:34 +0900

    StatementStrategy 인터페이스

---
+++ tobySpring/src/test/java/com/study/tobyspring/user/dao/StatementStrategyTest.java
@@ -0,0 +1,11 @@
+package com.study.tobyspring.user.dao;
+
+import org.junit.jupiter.api.Test;
+
+public class StatementStrategyTest {
+
+    @Test
+    void test() {
+        StatementStrategy statementStrategy = new StatementStrategy();
+    }
+}
