6161e1dd84c8c7162ad43ea9712ced36f6cbf315
Do-oya <qkrwjdgus1441@naver.com>
2025-02-19 22:49:37 +0900

    BufferedReaderCallback을 사용하는 템플릿 메소드

--- tobySpring/src/main/java/com/study/tobyspring/learningtest/template/Calculator.java
+++ tobySpring/src/main/java/com/study/tobyspring/learningtest/template/Calculator.java
@@ -31,4 +31,26 @@ public class Calculator {
             }
         }
     }
+
+    public Integer fileReadTemplate(String filePath, BufferedReaderCallback callback) throws IOException {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            int ret = callback.doSomethingWithReader(br);
+            return ret;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+        finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
 }
