68b9d2640df461fe4aae7f2213d42d041b6cf7f2
Do-oya <qkrwjdgus1441@naver.com>
2025-02-19 16:07:06 +0900

    add() 메소드의 PreparedStatement 생성 로직을 분리한 클래스

---
+++ tobySpring/src/main/java/com/study/tobyspring/user/dao/AddStatement.java
@@ -0,0 +1,18 @@
+package com.study.tobyspring.user.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AddStatement implements StatementStrategy {
+    @Override
+    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        return ps;
+    }
+}
