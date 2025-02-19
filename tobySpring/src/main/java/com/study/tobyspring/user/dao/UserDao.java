c01b12d662b7e11890047829e11e17df4ad608b9
Do-oya <qkrwjdgus1441@naver.com>
2025-02-19 15:25:42 +0900

    makeStatement()를 구현한 UserDao 서브클래스

--- tobySpring/src/main/java/com/study/tobyspring/user/dao/UserDao.java
+++ tobySpring/src/main/java/com/study/tobyspring/user/dao/UserDao.java
@@ -17,8 +17,7 @@ public class UserDao {
     public void add(User user) throws SQLException {
         Connection c = dataSource.getConnection();
 
-        PreparedStatement ps = c.prepareStatement(
-                "insert into users(id, name, password) values(?,?,?)");
+        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
         ps.setString(3, user.getPassword());
---
+++ tobySpring/src/test/java/com/study/tobyspring/user/dao/UserDaoDeleteAllTest.java
@@ -0,0 +1,11 @@
+package com.study.tobyspring.user.dao;
+
+import org.junit.jupiter.api.Test;
+
+public class UserDaoDeleteAllTest {
+
+    @Test
+    void test() {
+        UserDaoDeleteAll userDaoDeleteAll = new UserDaoDeleteAll();
+    }
+}
