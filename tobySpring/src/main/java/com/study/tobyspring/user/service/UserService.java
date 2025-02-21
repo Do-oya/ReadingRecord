4b01e726484b68ccd49fe2b7ae0410a17b70dc47
Do-oya <qkrwjdgus1441@naver.com>
2025-02-21 21:58:34 +0900

    5장 정리

--- tobySpring/src/main/java/com/study/tobyspring/user/service/UserService.java
+++ tobySpring/src/main/java/com/study/tobyspring/user/service/UserService.java
@@ -1,10 +1,33 @@
 package com.study.tobyspring.user.service;
 
 import com.study.tobyspring.user.dao.UserDao;
+import com.study.tobyspring.user.domain.Level;
+import com.study.tobyspring.user.domain.User;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class UserService {
     private final UserDao userDao;
 
+    public void upgradeLevels() {
+        User user = userDao.get("baby");
+        boolean changed;
+        if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+            user.setLevel(Level.SILVER);
+            changed = true;
+        }
+        else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+            user.setLevel(Level.GOLD);
+            changed = true;
+        }
+        else if (user.getLevel() == Level.GOLD) {
+            changed = false;
+        }
+        else {
+            changed = false;
+        }
+        if (changed) {
+            userDao.update(user);
+        }
+    }
 }
--- tobySpring/src/test/java/com/study/tobyspring/user/service/UserServiceTest.java
+++ tobySpring/src/test/java/com/study/tobyspring/user/service/UserServiceTest.java
@@ -1,20 +1,44 @@
 package com.study.tobyspring.user.service;
 
 import com.study.tobyspring.user.dao.DaoFactory;
+import com.study.tobyspring.user.dao.UserDao;
+import com.study.tobyspring.user.domain.Level;
+import com.study.tobyspring.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = DaoFactory.class)
 public class UserServiceTest {
     @Autowired
     UserService userService;
+    @Autowired
+    private UserDao dao;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        this.user = new User("baby", "아사", "monster", Level.GOLD, 1, 0);
+        dao.deleteAll();
+    }
 
     @Test
     void bean() {
+        assertThat(this.userService).isNotNull();
+    }
 
+    @Test
+    void upgradeLevels() {
+        dao.add(user);
+
+        userService.upgradeLevels();
+
+        assertThat(user.getLevel()).isEqualTo(Level.GOLD);
     }
 }
