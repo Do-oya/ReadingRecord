ac8625e20321d02cd20bc6834e240f11ff2c1b91
Do-oya <qkrwjdgus1441@naver.com>
2025-03-19 22:38:51 +0900

    필요없어진 변수 삭제

--- refactoring_2/src/main/java/chapter01/Statement.java
+++ refactoring_2/src/main/java/chapter01/Statement.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class Statement {
     private Invoice invoice;
-    private Map<String, Play> plays;
+    private static Map<String, Play> plays;
 
     public Statement(Invoice invoice, Map<String, Play> plays) {
         this.invoice = invoice;
@@ -21,18 +21,18 @@ public class Statement {
         NumberFormat formatter = NumberFormat.getCurrencyInstance(Locale.US);
 
         for (Invoice.Performance perf : invoice.getPerformances()) {
-            double thisAmount = amountFor(perf, playFor(plays, perf));
+            double thisAmount = amountFor(perf);
 
             // 포인트 적립
             volumeCredits += Math.max(perf.getAudience() - 30, 0);
 
             // 희극 관객 5명마다 추가 포인트
-            if ("comedy".equals(playFor(plays, perf).getType())) {
+            if ("comedy".equals(playFor(perf).getType())) {
                 volumeCredits += (int) Math.floor((double) perf.getAudience() / 5);
             }
 
             // 청구 내역 출력
-            result.append(String.format("%s: %s %d석\n", playFor(plays, perf).getName(), formatter.format(thisAmount / 100), perf.getAudience()));
+            result.append(String.format("%s: %s %d석\n", playFor(perf).getName(), formatter.format(thisAmount / 100), perf.getAudience()));
             totalAmount += thisAmount;
         }
 
@@ -42,14 +42,14 @@ public class Statement {
         return result.toString();
     }
 
-    private static Play playFor(Map<String, Play> plays, Invoice.Performance aPerformance) {
+    private static Play playFor(Invoice.Performance aPerformance) {
         return plays.get(aPerformance.getPlayID());
     }
 
-    private static double amountFor(Invoice.Performance aPerformance, Play play) {
+    private static double amountFor(Invoice.Performance aPerformance) {
         double result = 0;
 
-        switch (play.getType()) {
+        switch (playFor(aPerformance).getType()) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.getAudience() > 30) {
@@ -66,7 +66,7 @@ public class Statement {
                 break;
 
             default:
-                throw new IllegalArgumentException("알 수 없는 장르: " + play.getType());
+                throw new IllegalArgumentException("알 수 없는 장르: " + playFor(aPerformance).getType());
         }
         return result;
     }
